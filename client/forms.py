f05b1056c5a27ebf205098666bc166e6301fe431
muhamedoufi <muhamedoufi@gmail.com>
2022-02-26 12:41:54 +0000

    f

--- client/forms.py
+++ client/forms.py
@@ -5,4 +5,9 @@ class NewClient(forms.ModelForm):
     class Meta:
         model = Client
         fields ='__all__'  
-        exclude =['user','img']
+        exclude =['user','qrCode']
+    def __init__(self, *args, **kwargs):
+        super(NewClient, self).__init__(*args, **kwargs)
+        self.fields['email'].required=False 
+        self.fields['adresse'].required=False 
+        self.fields['tel'].required=False 
